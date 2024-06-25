cd9efc948d3639880533f5c43ba026d15d8be53a
luisalbeto <luis-alberto11@hotmail.com>
2024-06-25 17:49:27 -0400

    feat add logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    colors: {
+      'green': '#8FFF00',
+      'blue': '#460BFF',
+      'darkgreen': '#75B300',
+      'violet': '#3108B3',
+      'orange': '#FF5800',
+      'black': '#020617',
+      'white': '#FFFFFF',
+      'sky': '#CBCBFF',
+      'purple': '#7A08B3',
+      'yellow': '#CEFF08',
+      'bone': '#E9FFBF',
+      'pink': '#FF00CD',
+      'brick': '#B32A08',
+      'beige': '#FFBB50B',
+      'wine': '#B30090',
+      'lila': '#AE0BFF',
+      'transparent': '#00000000',
+      'turquesa': '#00FF66'
     },
+
   },
   plugins: [],
 };
